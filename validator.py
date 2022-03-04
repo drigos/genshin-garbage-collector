51a3ce0fc7a6339e0d2f912c6a3e6ed73aab99d1
André F. Martins <martins.adw@gmail.com>
2022-03-04 14:18:26 -0300

    fix: change inventory column size to 8

--- validator.py
+++ validator.py
@@ -34,9 +34,10 @@ def print_header(message, good_artifact_list, verbose=1):
     for good_artifact in good_artifact_list:
         formatted_artifact_list.append({k: v for k, v in good_artifact.items() if k in verbose_info[verbose_level]})
 
+    column_size = 8
     for formatted_artifact in formatted_artifact_list:
-        formatted_artifact['line'] = math.floor((formatted_artifact['Id'] / 7) + 1)
-        formatted_artifact['column'] = (formatted_artifact['Id'] % 7) + 1
+        formatted_artifact['line'] = math.floor((formatted_artifact['Id'] / column_size) + 1)
+        formatted_artifact['column'] = (formatted_artifact['Id'] % column_size) + 1
 
     print('-' * len(message))
     print(message)
